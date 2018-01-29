0d8e2f6d91b4d85060d41f957578a05e4eac3aac
Ovidiu Cara <ovidiu92@gmail.com>
2018-01-29 12:36:18 +0100

    Autocomplete now works The form will suggest pages belonging to the Art Movements Category

--- RAVE/forms.py
+++ RAVE/forms.py
@@ -2,6 +2,5 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    # TODO Resolve autocomplete
     search = forms.CharField(widget=forms.TextInput(attrs={'class':'autocomplete'}),
                              label='Search', max_length=50,  required=True, )
--- static/js/viewer-jquery.js
+++ static/js/viewer-jquery.js
@@ -79,7 +79,8 @@ $('document').ready(function () {
     //$('.parallax').parallax();
 
 });
-// TODO In order to get all images in their original resolution you need to make an
-// TODO API call for each image
-// TODO https://en.wikipedia.org/w/api.php?action=query&titles=File:Georges_Braque,_1909-10,_La_guitare_(Mandora,_La_Mandore),
-// TODO _oil_on_canvas,_71.1_x_55.9_cm,_Tate_Modern,_London.jpg&prop=imageinfo&iiprop=url&format=json
+/*  In order to get all images in their original resolution you need to make an
+    API call for each image
+    https://en.wikipedia.org/w/api.php?action=query&titles=File:Georges_Braque,_1909-10,_La_guitare_(Mandora,_La_Mandore),
+    _oil_on_canvas,_71.1_x_55.9_cm,_Tate_Modern,_London.jpg&prop=imageinfo&iiprop=url&format=json
+*/
